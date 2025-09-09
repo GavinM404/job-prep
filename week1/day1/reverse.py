d362e545df3daa16f16deccd0673dd78e3abd780
GavinM404 <gavin.musgrove87@gmail.com>
2025-09-08 21:07:10 -0500

    Added reverse and sum

---
+++ week1/day1/reverse.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+
+reversed_name = name[::-1]
+
+print(f"Reversed: {reversed_name}")
---
+++ week1/day1/sum.py
@@ -0,0 +1,6 @@
+number1 = int(input('Enter your first number! '))
+number2 = int(input('Enter your second number! '))
+
+total = number1 + number2
+
+print(f'{number1} + {number2} = {total}')
